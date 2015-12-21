29ad3340f810b6d93d0c98df327c8f331300c398
somkiat <somkiat.p@gmail.com>
2015-12-22 00:10:04 +0700

    Remove comment

--- src/main/java/api/servlet/HelloServlet.java
+++ src/main/java/api/servlet/HelloServlet.java
@@ -13,40 +13,39 @@ import org.codehaus.jackson.map.ObjectMapper;
 @WebServlet("/hello")
 public class HelloServlet extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		// resp.getWriter().print("Hello from Servlet 3");
-		
-		User user = new User();
-		user.setId("1");
-		user.setName("Up1");
-		
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.writeValue(resp.getOutputStream(), user);
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
-	}
+        User user = new User();
+        user.setId("1");
+        user.setName("Up1");
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(resp.getOutputStream(), user);
+
+    }
 
 }
 
 class User {
-	private String id;
-	private String name;
+    private String id;
+    private String name;
 
-	public String getId() {
-		return id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
 }
