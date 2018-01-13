05011207cee4d1bad3eab7ec5a377879aebf22f7
somkiat <somkiat.p@gmail.com>
2018-01-13 14:13:27 +0700

    Add test case

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,2 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class HelloTest {
+
+
+    @Test
+    public void xxxx() {
+        assertTrue(true);
+    }
+
 }
