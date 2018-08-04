e625686e9d17d53ae802b3258ca17c5edf8c526d
somkiat <somkiat.p@gmail.com>
2018-08-04 08:51:13 +0700

    Update test case and add apache maven

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,5 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
 public class HelloTest {
 
-
+    @Test
+    public void hello_first_test_case() {
+        assertTrue(true);
+//        fail("เขียนชุดการทดสอบสิ");
+    }
 
 }
